cc9c8e28bb612ba01ffd8159afb34e19ee416dd8
Faisal Wafa <faisalwafa10@gmail.com>
2019-11-01 13:48:21 +0700

    finish project SkorBasket

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,45 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+public class SharedScore extends AndroidViewModel {
+
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
+
+    public SharedScore(@NonNull Application application) {
+        super(application);
+        scoreHome = new MutableLiveData<>();
+        scoreVisitor = new MutableLiveData<>();
+        winner = new MutableLiveData<>();
+    }
+
+    public MutableLiveData<Integer> getScoreHome() {
+        return scoreHome;
+    }
+
+    public void setScoreHome(int score) {
+        scoreHome.setValue(score);
+    }
+
+    public MutableLiveData<Integer> getScoreVisitor() {
+        return scoreVisitor;
+    }
+
+    public void setScoreVisitor(Integer score) {
+        scoreVisitor.setValue(score);
+    }
+
+    public MutableLiveData<Boolean> getWinner() {
+        return winner;
+    }
+
+    public void setWinner(Boolean result) {
+        winner.setValue(result);
+    }
 }
